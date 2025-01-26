d2ce20096c1ce802f0251240c6a098e14545ce55
ori <ori@Ori-Cheung.local>
2025-01-26 19:36:28 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -17,9 +17,9 @@
 // Put your function here!
 fn calculate_price_of_apples(quantity: i32) -> i32 {
     if quantity > 40 {
-        return quantity;
+        quantity
     } else {
-        return quantity * 2;
+        quantity * 2
     }
 }
 
